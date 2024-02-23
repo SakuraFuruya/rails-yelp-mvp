8c2e320aea27b510e56cfa963ce409a10c360193
SakuraFuruya <cherryblossom.sakura.f@gmail.com>
2024-02-23 12:42:05 -0300

    add model with validation

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,7 @@
 class Restaurant < ApplicationRecord
-  # validates :name, :address, :category, presence: true
-  # CATEGORY = [ "chinese", "italian", "japanese", "french", "belgian" ]
-  # validates :category, inclusion: { in: SPECIES}
-  # When a restaurant is destroyed, all of its reviews must be destroyed as well.
+  has_many :reviews, dependent: :destroy
 
+  validates :name, :address, :category, presence: true
+  CATEGORY = [ "chinese", "italian", "japanese", "french", "belgian" ]
+  validates :category, inclusion: { in: CATEGORY }
 end
